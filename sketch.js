6f14fd3891d7f78039fb098b020d6ee19611df13
mike <michael.straeubig@plymouth.ca.uk>
2015-07-12 13:06:16 +0200

    added day/night, reorganised sounds

--- sketch.js
+++ sketch.js
@@ -1,4 +1,23 @@
-var cols, rows, tile_height, tile_width;
+"use strict";
+
+var animations = {}, blocks = [], cols, flower, grounds, rows, sky = [], sounds = {}, tile_height, tile_width;
+
+var block = {
+  w: 0,
+  h: 0,
+  col: 0,
+  row: 0,
+  sprite: null,
+  init: function() {
+    this.w = tile_width;
+    this.h = tile_height;
+    this.col = floor(random(cols));
+    this.row = floor(rows/2 - 2);
+    this.sprite = createSprite(placeX(this.col), placeY(this.row), this.w, this.h);
+    this.sprite.scale = 0.25;
+    this.sprite.addAnimation("block", animations.block);
+  }
+};
 
 var man = {
   col: 0,
@@ -7,15 +26,14 @@ var man = {
   h: 0,
   lives: 0,
   sprite: null,
-  animations: [],
   init: function() {
     this.w = tile_width;
     this.h = tile_height;
-    this.col = Math.floor(cols/2);
-    this.row = Math.floor(rows/2-1);
-    this.sprite = createSprite(positionX(this.col), positionY(this.row), this.w, this.h);
+    this.col = floor(cols/2);
+    this.row = floor(rows/2-1);
+    this.sprite = createSprite(placeX(this.col), placeY(this.row), this.w, this.h);
     this.sprite.scale = 0.25;
-    this.sprite.addAnimation("defaultman", this.animations[0]);
+    this.sprite.addAnimation("man", animations.man);
   },
   update: function() {
     if(keyWentDown("w")) {} else if (keyWentDown("s")) {}
@@ -25,9 +43,8 @@ var man = {
     } else if (keyWentDown("d")) {
       this.col = (this.col + 1) % cols;
     }
-    console.log(this.col)
-    this.sprite.position.x = positionX(this.col);
-    this.sprite.position.y = positionY(this.row);
+    this.sprite.position.x = placeX(this.col);
+    this.sprite.position.y = placeY(this.row);
     },
   };
 
@@ -38,48 +55,95 @@ var worm = {
   h: 0,
   lives: 0,
   sprite: null,
-  animations: [],
   init: function() {
     this.w = tile_width;
     this.h = tile_height;
-    this.col = Math.floor(cols/2 + 3);
-    this.row = Math.floor(rows/2 + 2);
-    this.sprite = createSprite(positionX(this.col), positionY(this.row), this.w, this.h);
+    this.col = floor(cols/2 + 3);
+    this.row = floor(rows/2 + 2);
+    this.sprite = createSprite(placeX(this.col), placeY(this.row), this.w, this.h);
     this.sprite.scale = 0.25;
-    this.sprite.addAnimation("defaultworm", this.animations[0]);
+    this.sprite.addAnimation("defaultworm", animations.worm);
   },
   update: function() {
     if(keyWentDown("i")) {
-      this.row = Math.max(this.row - 1, Math.floor(rows/2));
+      this.row = max(this.row - 1, floor(rows/2));
     } else if (keyWentDown("k")) {
-      this.row = Math.min(this.row + 1, rows - 1);
+      this.row = min(this.row + 1, rows - 1);
         }
     if (keyWentDown("j")) {
-      this.col = Math.max(this.col - 1, 0);
+      this.col = max(this.col - 1, 0);
     } else if (keyWentDown("l")) {
-      this.col = Math.min((this.col + 1), cols-1);
+      this.col = min((this.col + 1), cols-1);
     }
-    console.log(this.col)
-    this.sprite.position.x = positionX(this.col);
-    this.sprite.position.y = positionY(this.row);
+    this.sprite.position.x = placeX(this.col);
+    this.sprite.position.y = placeY(this.row);
     },
   };
 
 function preload() {
-  man.animations.push(loadAnimation("assets/hero.png"));
-  worm.animations.push(loadAnimation("assets/worm.png"));
+  sounds.day = loadSound("snd/ManVsWorm-Day_30sec_128bpm.mp3");
+  sounds.night = loadSound("snd/ManVsWorm-Night_30sec_128bpm.mp3");
+  sounds.manwins = loadSound("snd/ManVsWorm-Manwin.mp3");
+  sounds.wormwins = loadSound("snd/ManVsWorm-Wormwin.mp3");
+  sounds.pickupfood = loadSound("snd/ManVsWorm-WormswallowsFood.mp3");
+  sounds.dighole = loadSound("snd/ManVsWorm-Wormdiggahole.mp3");
+  sounds.pickupblock = loadSound("snd/ManVsWorm-Manpickup.mp3");
+  sounds.buildblock = loadSound("snd/ManVsWorm-Manbuildup.mp3");
+  sounds.movedown = loadSound("snd/ManVsWorm-Housefallsdown.mp3");
+
+  animations.man = loadAnimation("img/hero.png");
+  animations.worm = loadAnimation("img/worm.png");
+  animations.block = loadAnimation("img/house_block.png");
+  animations.ground = loadAnimation("img/ground.png");
+  animations.groundTop = loadAnimation("img/ground_top.png");
+  animations.plant = loadAnimation("img/ground_flower.png");
 }
 
 function setup() {
-  var ratio = Math.sqrt(3)/2;
+  var c, ground, i, r, ratio = sqrt(3)/2, startRow;
+
   createCanvas(960, 640 * ratio);
   tile_width = 64;
   tile_height = tile_width *ratio;
-  cols = Math.floor(width / tile_width);
-  rows = Math.floor(height / tile_height);
-  console.log(cols +","+ rows);
+  cols = floor(width / tile_width);
+  rows = floor(height / tile_height);
+  console.log("cols, rows: " + cols +", "+ rows); // DEBUG
+
+  for (i = 0; i < cols; i = i + 1) {
+    blocks[i] = 0;
+  }
+
+  grounds = new Group();
+
+  startRow = floor(rows/2-1);
+  for(c = 0; c < cols; c = c + 1) {
+    for(r = floor(rows/2-1); r < rows; r = r + 1) {
+      ground = createSprite(placeX(c), placeY(r));
+      if (r === startRow) {
+        ground.addAnimation("ground", animations.groundTop);
+      } else {
+        ground.addAnimation("ground", animations.ground); }
+      ground.scale = 0.5;
+      grounds.add(ground);
+    }
+  }
+
+  var fx = floor(random(startRow, rows)) // DEBUG
+  var fy = floor(random(0, cols)) // DEBUG
+  console.log("flower: " + fx +", "+ fy);
+  console.log("startRow: " + startRow);
+
+  flower = createSprite(
+    placeX(fx),
+    placeY(fy)
+  );
+  flower.scale = 0.5;
+  flower.addAnimation("flower", animations.plant);
+
   man.init();
-  worm.init()
+  worm.init();
+
+  nextDay(1, 5);
 }
 
 function draw() {
@@ -94,11 +158,11 @@ function draw() {
   ellipse(width/2, height/2, 5, 5); // DEBUG
 }
 
-function positionX(column) {
+function placeX(column) {
   return (column/cols) * width + tile_width/2;
 }
 
-function positionY(row) {
+function placeY(row) {
   return (row/rows) * height;
 }
 
@@ -111,3 +175,26 @@ function drawGrid() {
     line(0, n, width, n);
   }
 }
+
+function addBlock() {
+  var b = Object.create(block);
+  b.init();
+}
+
+function nextDay(day, timespanSecs) {
+  setTimeout(function () {
+    if (sounds.day.isPlaying()) sounds.day.stop();
+    nextNight(day, timespanSecs);
+  }, timespanSecs * 1000);
+  // sounds.day.play();
+  addBlock();
+}
+
+function nextNight(day, timespanSecs) {
+  setTimeout(function () {
+    if (sounds.night.isPlaying()) sounds.night.stop();
+    nextDay(day + 1, timespanSecs);
+  }, timespanSecs * 1000);
+  // sounds.night.play();
+}
+
